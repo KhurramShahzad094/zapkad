10b64313d925ad872700ea9a48fb4486d5b57b5d
Muhammd Khurram Shahzad <khurram.shahzad094@gmail.com>
2022-07-04 01:28:08 +0500

    mappers added

---
+++ app/src/main/java/com/khurram/zapkadtest/data/mapper/UserDetailMapper.kt
@@ -0,0 +1,15 @@
+package com.khurram.zapkadtest.data.mapper
+
+import com.khurram.zapkadtest.data.model.UserDetailEntity
+import com.khurram.zapkadtest.data.network.model.UserDetailRemote
+
+fun UserDetailRemote.toUserDetailEntity() = UserDetailEntity(
+    login = login!!,
+    avatar_url = avatarUrl,
+    name = name,
+    company = company,
+    blog = blog,
+    followers = followers,
+    following = following,
+)
+
